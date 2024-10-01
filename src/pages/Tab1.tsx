bf0ecc1beb50b228013965b6972c6082a8d55e3e
wibikamka <nurwibisanakamka@gmail.com>
2024-10-01 21:11:46 +0700

    first edit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,6 +16,27 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+        
+        {/* Tambahkan Daftar */}
+        <IonList>
+          <IonItem button>
+            <IonLabel>Item 1</IonLabel>
+          </IonItem>
+          <IonItem button>
+            <IonLabel>Item 2</IonLabel>
+          </IonItem>
+          <IonItem button>
+            <IonLabel>Item 3</IonLabel>
+          </IonItem>
+        </IonList>
+
+        {/* Tambahkan Beberapa Tombol */}
+        <div style={{ padding: '16px' }}>
+          <IonButton expand="full" color="primary">Tombol 1</IonButton>
+          <IonButton expand="full" color="secondary" style={{ marginTop: '8px' }}>Tombol 2</IonButton>
+        </div>
+
+        {/* Komponen Eksplorasi */}
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
